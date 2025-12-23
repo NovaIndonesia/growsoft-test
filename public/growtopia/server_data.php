71a2d2a347f856718711786db4a1c25d43fe7df6
Nova <82694530+NovaIndonesia@users.noreply.github.com>
2025-12-23 17:12:56 +0700

    Create server_data.php

---
+++ public/growtopia/server_data.php
@@ -0,0 +1,23 @@
+server|5.39.13.28
+port|17455
+loginurl|5.39.13.28
+type|1
+beta_server|beta.growtopiagame.com
+beta_loginurl|beta.growtopiagame.com
+beta_port|26999
+beta_type|1
+beta2_server|beta2.growtopiagame.com
+beta2_loginurl|beta2.growtopiagame.com
+beta2_port|26999
+beta2_type|1
+beta3_server|34.202.7.77
+beta3_loginurl|beta3.growtopiagame.com
+beta3_port|26999
+beta3_type|1
+type2|1
+#maint|Server is under maintenance. We will be back online shortly. Thank you for your patience!
+meta|dedi_355
+error|999
+Please Wait Connecting To `2GrowRush`o Private Server
+RTENDMARKERBS1001*
+shutting down connection #0
