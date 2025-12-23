873f6bba85279817a4ff6921cb74120f5ab08e41
Nova <82694530+NovaIndonesia@users.noreply.github.com>
2025-12-23 17:36:35 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const path = require("path");
 
 const app = express();
 
+// 🔑 Gunakan express.static untuk melayani file statis dari folder htdocs
+app.use(express.static(path.join(__dirname, "htdocs")));
+
+// Route khusus untuk /growtopia/server_data.php (jika masih ingin dikustom)
 app.get("/growtopia/server_data.php", (req, res) => {
   const filePath = path.join(__dirname, "htdocs", "growtopia", "server_data.php");
 
@@ -12,20 +16,29 @@ app.get("/growtopia/server_data.php", (req, res) => {
       return res.status(404).send("server_data.php not found");
     }
 
-    // Set header agar konten ditampilkan di browser
     res.status(200);
-    res.setHeader("Content-Type", "text/html; charset=utf-8"); // Ubah ke text/html jika isi file HTML/PHP
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
     res.setHeader("Content-Disposition", "inline");
     res.send(data);
   });
 });
 
-// Tolak semua request lain
-app.use((req, res) => {
-  res.status(403).send("403 Forbidden");
+// Route untuk root ("/") agar menampilkan index.html (jika ada)
+app.get("/", (req, res) => {
+  const filePath = path.join(__dirname, "htdocs", "index.html");
+
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      return res.status(404).send("index.html not found");
+    }
+
+    res.status(200);
+    res.setHeader("Content-Type", "text/html; charset=utf-8");
+    res.send(data);
+  });
 });
 
-// Jalankan di port 80 (HTTP)
-app.listen(80, () => {
+// Jalankan server di port 80
+app.listen(443, () => {
   console.log("Server running on port 80");
 });
