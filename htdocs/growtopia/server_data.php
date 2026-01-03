64ff770a03935df33c907fb0d049e09bf401348e
Nova <82694530+NovaIndonesia@users.noreply.github.com>
2026-01-03 15:52:33 +0700

    Update server_data.php

--- htdocs/growtopia/server_data.php
+++ htdocs/growtopia/server_data.php
@@ -1,23 +1,11 @@
-server|5.39.13.28
-port|17455
-loginurl|5.39.13.28
+server|15.235.166.218
+port|17091
 type|1
-beta_server|beta.growtopiagame.com
-beta_loginurl|beta.growtopiagame.com
-beta_port|26999
+#maint|Server currently change hosting, please join discord.gg/topps to get the latest host.
+loginurl|15.235.166.218
+beta_server|43.159.38.250
+beta_port|17091
+
 beta_type|1
-beta2_server|beta2.growtopiagame.com
-beta2_loginurl|beta2.growtopiagame.com
-beta2_port|26999
-beta2_type|1
-beta3_server|34.202.7.77
-beta3_loginurl|beta3.growtopiagame.com
-beta3_port|26999
-beta3_type|1
-type2|1
-#maint|Server is under maintenance. We will be back online shortly. Thank you for your patience!
-meta|dedi_355
-error|999
-Please Wait Connecting To `2GrowRush`o Private Server
-RTENDMARKERBS1001*
-shutting down connection #0
+meta|localhost
+RTENDMARKERBS1001
