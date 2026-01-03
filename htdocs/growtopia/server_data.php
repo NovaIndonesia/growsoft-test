31e6842ce3e7857f225f4ff92fac753a227b8a14
Nova <82694530+NovaIndonesia@users.noreply.github.com>
2026-01-03 16:05:48 +0700

    Update server_data.php

--- htdocs/growtopia/server_data.php
+++ htdocs/growtopia/server_data.php
@@ -1,11 +1,21 @@
 server|15.235.166.218
-port|17091
-type|1
-maint|Connected to server
+port|17455
 loginurl|15.235.166.218
-beta_server|43.159.38.250
-beta_port|17091
-
+type|1
+beta_server|beta.growtopiagame.com
+beta_loginurl|beta.growtopiagame.com
+beta_port|26999
 beta_type|1
-meta|localhost
+beta2_server|beta2.growtopiagame.com
+beta2_loginurl|beta2.growtopiagame.com
+beta2_port|26999
+beta2_type|1
+beta3_server|34.202.7.77
+beta3_loginurl|beta3.growtopiagame.com
+beta3_port|26999
+beta3_type|1
+type2|1
+maint|Connected to server
+meta|dedi_355
+error|999
 RTENDMARKERBS1001
